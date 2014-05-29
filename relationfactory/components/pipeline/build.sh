6a80b3cb87d047eae1753ddfcba51a3627f514df
Vidhoon Viswanathan <vidhoon@cs.utexas.edu>
2014-05-28 19:06:37 -0500

    Modified build.sh for components
    Changed path settings for JAVA, ANT and CLASSPATH
    to build the system components.
    
    Signed-off-by: Vidhoon Viswanathan <vidhoon@cs.utexas.edu>

--- relationfactory/components/pipeline/build.sh
+++ relationfactory/components/pipeline/build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+
+#
+# RUNS ON LOCAL MACHINE
+# TO BE BUILT ON LOCAL MACHINE AND THEN COPIED TO CLUSTER
+# BECAUSE CABAL IS NOT INSTALLED ON CLUSTER 
+#
+
 cd $TAC_ROOT/components/pipeline
-#JAVA_HOME=$TAC_ROOT/lib/java/jdk1.6.0_18/
-LOCALCLASSPATH=$JAVA_HOME/lib/tools.jar:./lib/antlib/*
-ANT_HOME=./lib/antlib
+JAVA_HOME="/usr/lib/jvm/java-7-oracle"
+LOCALCLASSPATH="$JAVA_HOME/lib/tools.jar:/usr/share/ant/lib/*"
+ANT_HOME=/usr/share/ant/
 $JAVA_HOME/bin/java -Dant.home=$ANT_HOME -classpath $LOCALCLASSPATH org.apache.tools.ant.Main $*
